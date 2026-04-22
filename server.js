d1baf1382aac2cfd1ac1923b9777d18c60a89a7f
Natalia Jabinschi <natalia.jabinschi@umontreal.ca>
2026-04-22 09:54:14 -0400

    fix: réponse 204 aux requêtes OPTIONS (preflight CORS) et ajout du script npm run package

--- server.js
+++ server.js
@@ -45,10 +45,11 @@ app.use((req, _res, next) => {
 });
 
 // CORS
-app.use((_req, res, next) => {
+app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', process.env.CORS_ORIGIN || '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  if (req.method === 'OPTIONS') return res.sendStatus(204);
   next();
 });
 
