9e4bef878966abf3d4ae2d728d7ceda95b63433a
Natalia Jabinschi <natalia.jabinschi@umontreal.ca>
2026-05-14 14:27:31 -0400

    Modifications mineurs

--- server.js
+++ server.js
@@ -90,7 +90,7 @@ app.get('/health', async (_req, res) => {
   const status = {
     status: 'OK',
     env: process.env.NODE_ENV || 'development',
-    message: 'Gestion des achats',
+    message: 'API Gestion des achats',
     timestamp: new Date().toISOString()
   };
   try {
